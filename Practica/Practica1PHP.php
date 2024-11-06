ba2a60adea0fc5409bed9374a35dfba8920547ca
Enrique Alejandro Villada Navarrete <101196855+ANobody09@users.noreply.github.com>
2024-11-06 15:16:30 -0500

    Add files via upload

---
+++ Practica/Practica1PHP.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<tittle> Practica 1 php </title>
+</head>
+<body>
+<?php
+
+ECHO "Hola! Este es mi primer scrip en PHP<BR>";
+
+//Esto es una linea de comentario
+
+#Esto también es una linea de comentario
+
+/*Esto es un 
+bloque de mas de una linea
+de comentarios*/
+
+$vartext="Hola! Ahora estoy usando variables para guardar texto<BR>";
+echo $vartext;
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "El numero de la suma de $varnum1 y $varnum2 es = $res";
+?>
+</body>
+</html>
